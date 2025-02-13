b1091da3be60964623564a3f5513929241e370bc
dineo-matla <dineomatla@outlook.com>
2025-02-13 14:46:09 +0200

    Added weather API URL & Key. Created a function to fetch data

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+function searchCity(city){
+  let apiKey = "98cf17tbce8o72b5edd6a39d2d044324";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+console.log(city);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#text-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  city = searchInput.value; 
 }
 
 let textFormElement = document.querySelector("#text-form");
